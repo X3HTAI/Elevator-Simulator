0d81f48a12759514903e3a063786b932032cb1e3
X3HTAI <dvk04@mail.ru>
2025-02-16 13:10:32 +0300

    Update Симулятор_лифта(ООП).cpp

--- code_folder/Симулятор_лифта(ООП).cpp
+++ code_folder/Симулятор_лифта(ООП).cpp
@@ -148,4 +148,117 @@ public:
         }
         if (wcs == 'Y' || wcs == 'y') {
             cout << "Сколько этажей в доме? (15 по стандарту)" << endl;
-            while (
+            while (totalFloors < 2) {
+                cout << "(Не меньше 2)" << endl << "Этажей: ";
+                cin >> totalFloors;
+            }
+            cout << "На каком этаже вы сейчас? (1 по стандарту)" << endl;
+            while (currentFloor < 1 || currentFloor > totalFloors) {
+                cout << "(Не меньше 1 и не больше кол-ва этажей)" << endl << "Этаж: ";
+                cin >> currentFloor;
+            }
+            cout << "На каком этаже лифт сейчас? (1 по стандарту)" << endl;
+            while (elevatorFloor < 1 || elevatorFloor > totalFloors) {
+                cout << "(Не меньше 1 и не больше кол-ва этажей)" << endl << "Этаж: ";
+                cin >> elevatorFloor;
+            }
+            char wces = 'o';
+            cout << "Хотите ли вы внести изменения в настройки лифта?" << endl;
+            while (wces != 'N' && wces != 'n' && wces != 'Y' && wces != 'y') {
+                cout << "(Y или y - Да, N или n - Нет)" << endl;
+                cin >> wces;
+            }
+            if (wces == 'Y' || wces == 'y') {
+                diameterOfTheWhinchDrum = 0;
+                floorHeight = 0;
+                cout << "Какой диаметр у барабана лебедки лифта в метрах? (0.2 метра по стандарту)" << endl;
+                while (diameterOfTheWhinchDrum <= 0) {
+                    cout << "(Не меньше или равно 0)" << endl << "Диаметр в метрах: ";
+                    cin >> diameterOfTheWhinchDrum;
+                }
+                cout << "Какая высота у этажей в метрах? (3 метра по стандарту)" << endl;
+                while (floorHeight < 2.5) {
+                    cout << "(Не меньше 2.5)" << endl << "Высота в метрах: ";
+                    cin >> floorHeight;
+                }
+            }
+        }
+    }
+
+    int getTotalFloors() const {
+        return totalFloors;
+    }
+
+    int getCurrentFloor() const {
+        return currentFloor;
+    }
+
+    int getElevatorFloor() const {
+        return elevatorFloor;
+    }
+
+    double getDiameterOfTheWhinchDrum() const {
+        return diameterOfTheWhinchDrum;
+    }
+
+    double getFloorHeight() const {
+        return floorHeight;
+    }
+};
+
+class ElevatorSystem {
+private:
+    Elevator elevator;
+    Door door;
+    ElevatorSettings settings;
+
+public:
+    ElevatorSystem() : settings() {
+        settings.configureSettings();
+        elevator = Elevator(settings.getDiameterOfTheWhinchDrum(), settings.getFloorHeight(), settings.getTotalFloors(), settings.getCurrentFloor());
+    }
+
+    void callElevator(int targetFloor) {
+        if (targetFloor < 1 || targetFloor > settings.getTotalFloors()) {
+            cout << "В этом доме нет такого этажа." << endl;
+            return;
+        }
+
+        // Создаем команды
+        OpenDoorCommand openDoorCommand(door);
+        MoveElevatorCommand moveElevatorCommand(elevator, targetFloor);
+        CloseDoorCommand closeDoorCommand(door);
+
+        // Выполняем команды
+        openDoorCommand.execute();
+        moveElevatorCommand.execute();
+        closeDoorCommand.execute();
+    }
+
+    int getTotalFloors() const {
+        return settings.getTotalFloors();
+    }
+};
+
+int main() {
+    cout << "Симулятор лифта" << endl;
+
+    ElevatorSystem elevatorSystem;
+
+    char wue = 'y';
+    while (wue == 'y' || wue == 'Y') {
+        int targetFloor;
+        cout << "На какой этаж вы хотите попасть? (1-" << elevatorSystem.getTotalFloors() << ")" << endl;
+        cin >> targetFloor;
+
+        elevatorSystem.callElevator(targetFloor);
+
+        cout << "Хотите использовать лифт снова? (Y/N)" << endl;
+        cin >> wue;
+    }
+
+    cout << "Вы вышли из лифта." << endl;
+
+    return 0;
+}
+
